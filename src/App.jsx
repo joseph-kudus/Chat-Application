cd47927ec2ab7d1e30f16f98ecca89a3e6b8693b
Joseph <josephlukudu22@gmail.com>
2026-04-07 00:23:12 +0300

    added content to the asidebar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Route, Routes } from "react-router";
-import Landingpage from "./component/Landingpage";
+import { Route, Routes } from "react-router-dom";
+import DashboardLayout from "./layouts.jsx/DashboardLayout.jsx";
+
 
 function App() {
   return (
     <div className="md-4 color-red bg-indigo-700 text-3xl underline">
       <Routes>
-        <Route path="/" element={<Landingpage />} />
+        <Route path="/" element={<DashboardLayout/>}/>
       </Routes>
     </div>
   );
--- src/layouts.jsx/DashboardLayout.jsx
+++ src/layouts.jsx/DashboardLayout.jsx
@@ -2,21 +2,54 @@ import React from "react";
 import "../styles/dashboardlayout.css";
 import logohead from "../assets/Image/mingcute_chat-4-fill.png";
 import { Link } from "react-router";
+import { DotIcon, Phone, PhoneCall, Plus, Search, SmileIcon, Video } from "lucide-react";
 
 function DashboardLayout() {
   return (
     <div className="dashboardlayout">
       <div className="container">
         <aside className="drawer">
-          <div className="logo-wrapper">
+          <div className="logo-wraper">
             <img src={logohead} alt="" />
-            <span>Chattes</span>
+            <span>Chatties</span>
+          </div>
+          <div className="search-bar">
+            <Search />
+            <input type="text" placeholder="Search messages, people" />
+            <button>
+              <span>
+                <Plus className="bg-indigo-900" />
+              </span>
+            </button>
           </div>
         </aside>
         <div className="content-wrap">
-          <div className="header">header</div>
+          <div className="header">
+            <div className="header-nav">
+              <div className="user-profile">Grace Miller</div>
+              <div className="calls-tap">
+                <Phone />
+                <Video />
+                <DotIcon />
+              </div>
+            </div>
+          </div>
           <main className="main">main content</main>
-          <footer className="footer">footer</footer>
+          <footer className="footer">
+            <div className="footer-wraper">
+              <div className="message-area">
+                <span>
+                  <SmileIcon />
+                </span>
+                <input type="text" placeholder="Type message" />
+              </div>
+              <div className="attach-area">
+                <i class="fa-solid fa-user"></i>
+                <p>Send</p>
+                <i class="fa-regular fa-user"></i>
+              </div>
+            </div>
+          </footer>
         </div>
       </div>
     </div>
